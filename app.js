011c38ffa0c2f239b9a069674a0cea8e8b0672f4
Jerry Gao <gao@blackwright.com>
2017-02-13 20:55:44 -0500

    finish displaying request/response data

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const http = require("http");
+const fs = require("fs");
 
 const hostname = "127.0.0.1";
 const port = 3000;
+const path = "./public/index.html";
 
 const server = http.createServer( (req, res) => {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello World\n");
+  fs.readFile(path, "utf8", (err, data) => {
+    if (err) {
+      res.writeHead(400, {"Content-Type": "text/plain"});
+      res.end("404 Not Found");
+    } else {
+
+      let myReq = {};
+      ["url", "method", "httpVersion", "headers"].forEach( (key) => {
+        myReq[key] = req[key];
+      });
+      myReq = JSON.stringify(myReq, null, 2);
+      let myRes = {};
+      ["statusMessage", "statusCode", "_header"].forEach( (key) => {
+        myRes[key] = res[key];
+      });
+      myRes = JSON.stringify(myRes, null, 2);
+
+      res.writeHead(200, {"Content-Type": "text/html"});
+      data = data.replace("{{ req }}", myReq);
+      data = data.replace("{{ res }}", myRes);
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
