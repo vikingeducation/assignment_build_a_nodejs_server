5764ee2172ecca6feaa52ee40e2a87d8400cf733
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-20 12:19:55 +0000

    Make public dir and index.html. app.js now using fs to read file

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 "use strict";
 
 const http = require("http");
+const fs = require("fs");
+
+
 const path = "0.0.0.0";
 const port = 8080;
 
 
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "content-Type" : "text/plain"
+    fs.readFile("./public/index.html", "utf8", (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end("Soz, that file could not be found.");
+            
+        } else {
+            res.writeHead(200, {
+            "content-Type" : "text/html"
+            });
+            res.end(data);
+        }
     });
-    res.end("You got it");
+
 });
 
 
