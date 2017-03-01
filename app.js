6ac079a37697d6cb287456cafd76a07df25100e8
Egle Libby <egle.ciparyte@gmail.com>
2017-03-01 11:16:03 -0500

    display the request and response data

--- app.js
+++ app.js
@@ -7,13 +7,25 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   fs.readFile('./public/index.html', 'utf8', (err, data) => {
     if (err) {
-      res.writeHead(404);
+      res.writeHead(404, {
+        'Content-Type': "text/plain"
+      });
       res.end("404 File Not Found");
     } else {
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
-      res.end(data);
+      var my_req = {};
+      var req_array = ["url", "method", "httpVersion", "headers"];
+      req_array.forEach(function(key) {
+        my_req[key] = req[key];
+      });
+      var my_res = {};
+      var res_array = ["statusMessage", "statusCode", "_header"];
+      res_array.forEach(function(key) {
+        my_res[key] = res[key];
+      });
+      res.end(data.replace("{{ req }}", JSON.stringify(my_req, null, 2)).replace("{{ res }}", JSON.stringify(my_res, null, 2)));
     }
   });
 });
