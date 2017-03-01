8807e37e8131831a1524a6199c6bc3ed9559eb77
Egle Libby <egle.ciparyte@gmail.com>
2017-03-01 10:41:05 -0500

    output HTML

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
+  fs.readFile('./public/index.html', 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 File Not Found");
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data);
+    }
   });
-  res.end("Hello World!");
 });
 
 server.listen(port, hostname, function() {
