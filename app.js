1f0ba74e45a17149eafd2fbfc49f8f0a3583e91d
Ann Allan <ann.cristina.allan@gmail.com>
2017-03-05 15:05:25 -0500

    Display Request and Response Data

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var fs = require('fs');
+var req = require('./lib/req');
+var res = require('./lib/res');
 const http = require('http');
 
 const port = 3000;
 const host = 'localhost';
 const server = http.createServer(function (req, res) {
   fs.readFile('./public/index.html', 'utf8', function (err, data) {
+    var data = String.prototype.replace(data, JSON.stringify(data, null, 2));
     if (err) {
       res.writeHead(404);
       res.end('404 not found');
