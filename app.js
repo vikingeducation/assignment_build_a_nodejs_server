dec1127de59b3b9293a68e48cfad324d1d7e2ec8
Adam Jazairi <adamjazairi@gmail.com>
2017-03-15 16:15:17 -0400

    Display request/response data in HTML

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 var http = require('http');
+var fs = require('fs');
 
 var port = 3000;
 var host = 'localhost';
 
+
 var server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World!');
+  // Initialize objects for results and requests
+  var results = {};
+  var requests = {};
+
+  fs.readFile('./public/index.html', 'utf-8', function(err, data) {
+    // Handle all errors as 404
+    if (err) {
+      res.statusCode = 404;
+      res.end(data);
+    }
+    else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+
+      // Set properties in requests and results objects
+      requests.url = req.url;
+      requests.method = req.method;
+      requests.httpVersion = req.httpVersion;
+      requests.headers = req.headers;
+      results.statusCode = res.statusCode;
+      results.statusMessage = res.statusMessage;
+      results._header = res._header;
+
+      // Stringify objects and replace placeholder strings in HTML with JSON
+      requests = JSON.stringify(requests, null, 2);
+      results = JSON.stringify(results, null, 2);
+      data = data.replace("{{ req }}", requests);
+      data = data.replace("{{ res }}", results);
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, host, () => {
