8c6ed784abd7f05c1887125a6fc89c1efb530880
nicoasp <nicolas.amaya@gmail.com>
2017-03-17 17:53:15 +0100

    finish outputting html

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 var host = "localhost"
 var port = 3000;
 
 server = http.createServer(function(req, res){
-	res.writeHead(200, {
-		"Content-Type": "text"
+	fs.readFile("./public/index.html", "utf8", function(err, data){
+		if (err) {
+			res.writeHead(404);
+			res.end("404 Not Found");
+		} else {
+			res.writeHead(200, {
+				"Content-Type": "text/html"
+			});
+			res.end(data);
+		}
 	});
-	res.end("Hello World!");
 });
 
 server.listen(port, host, function(){
