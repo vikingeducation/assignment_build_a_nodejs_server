6722d4610f6b0615872b7c214cf75b3636e534d9
William Whitworth <william10charles@gmail.com>
2017-03-19 10:10:04 -0600

    server that reads and serves html files

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var http = require("http");
+var fs = require("fs");
 
 var port = 3000;
 var host = "localhost";
 
 var server = http.createServer(function(request, response) {
-  response.writeHead(200, {
-    "Content-type": "text/html"
+  fs.readFile("./public/index.html", "utf8", function(err, data) {
+    if (err) {
+      response.writeHead(404, {
+        "Content-type": "text/html"
+      });
+      response.end("error 404");
+    } else {
+      response.writeHead(200, {
+        "Content-type": "text/html"
+      });
+      response.end(data);
+    }
   });
-  response.end("Hello world.");
 });
 
 server.listen(port, host, function() {
-  console.log("Listening at http://${host}:${port}");
+  console.log("Listening at http://" + host + ":" + port);
 });
