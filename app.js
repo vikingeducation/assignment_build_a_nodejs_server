f765c12349e68f30ea0df0cc649c5f5a6eac6e31
tim5046 <tim5046@gmail.com>
2017-03-19 19:08:25 -0700

    serve index.html

--- app.js
+++ app.js
@@ -7,10 +7,17 @@ var host = 'localhost';
 
 
 var server = http.createServer(function(req, res) {
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
       res.writeHead(200, {
         "Content-Type": "text/html"
       });
-      res.end('Hello World');
+      res.end(data);
+    }
+  });
 });
 
 
