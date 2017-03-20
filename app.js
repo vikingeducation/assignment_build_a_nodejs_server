9de1337d5800d5df33d748a6b94cfa075c0a42a4
tim5046 <tim5046@gmail.com>
2017-03-19 19:28:08 -0700

    page render with response and request

--- app.js
+++ app.js
@@ -15,11 +15,32 @@ var server = http.createServer(function(req, res) {
       res.writeHead(200, {
         "Content-Type": "text/html"
       });
-      res.end(data);
+      res.end(serverUtils.parseVariables(data, req, res));
     }
   });
 });
 
+var serverUtils = {
+  parseVariables: function(data, req, res){
+    var reqObj = {
+      url: req.url,
+      method: req.method,
+      httpVersion: req.httpVersion,
+      headers: req.headers
+    };
+
+    var resObj = {
+      statusMessage: res.statusMessage,
+      statusCode: res.statusCode,
+      _header: res._header
+    }
+
+    data = data
+      .replace(/{{ req }}/g, JSON.stringify(reqObj, null, 2))
+      .replace(/{{ res }}/g, JSON.stringify(resObj, null, 2))
+    return data
+  }
+}
 
 server.listen(port, host, function() {
   console.log(`Listening at http://${ host }:${ port }`);
