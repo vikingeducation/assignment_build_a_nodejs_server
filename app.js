0eec1723a1d7a0ca5a01b5ea93a7fed016df4f56
tim5046 <tim5046@gmail.com>
2017-03-19 19:06:14 -0700

    basic setup

---
+++ app.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var fs = require('fs');
+
+
+var port = 3000;
+var host = 'localhost';
+
+
+var server = http.createServer(function(req, res) {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end('Hello World');
+});
+
+
+server.listen(port, host, function() {
+  console.log(`Listening at http://${ host }:${ port }`);
+});
+
+
+
+
+
+
+
