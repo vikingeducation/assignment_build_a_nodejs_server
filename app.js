6827414b74fcec62fdf9b946aefa52b9f839c2ee
Chris Banning <chrisbanning510@gmail.com>
2017-03-21 21:32:55 -0700

    served HTML using simple node server

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 const host = '127.0.0.1';
 const port = 3000;
 
 var server = http.createServer(function(req, res) {
-
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello yo World\n');
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      req.statusCode = 200;
+      req.end("4004 no found!");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, host, function() {
