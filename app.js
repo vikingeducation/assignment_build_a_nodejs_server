15283ae41082315079009cc5a51c8ee73a2c7eb4
markmarkyesyes <markhahn545@gmail.com>
2017-03-21 23:16:03 -0500

    serves html

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+const fs = require('fs');
 const http = require('http');
 const host = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": 'text/plain'
-    });
-  res.end('Hello World');
-})
+  fs.readFile('./public/index.html', "UTF-8", (err, data) => {
+    res.writeHead(200, {
+      "Content-Type": 'text/html'
+      });
+    JSON.stringify({"test": "TEST"}, null, 2);
+    res.end(data);
+  });
+});  
 
 server.listen(port, host, function() {
   console.log(`Server listening at http://${host}:${port}/`
