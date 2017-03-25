b7fd0b6bf70fc71e56d2f65f76eed98ec8a0fbb7
deven1 <deven@sloan.mit.edu>
2017-03-25 11:30:54 -0400

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.end('Hello World\n');
+  fs.readFile('./public/index.html', 'utf8', function(err, data){
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    }
+    else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
