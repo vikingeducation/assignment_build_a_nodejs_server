d09c17d7c675e3a9e8e419d55f9df5b175294fba
deven1 <deven@sloan.mit.edu>
2017-03-25 22:07:33 -0400

    finishing the assignment

--- app.js
+++ app.js
@@ -9,49 +9,51 @@ const server = http.createServer((req, res) => {
   // res.setHeader('Content-Type', 'text/plain');
   // res.end('Hello World\n');
   fs.readFile('./public/index.html', 'utf8',
-                      function(err, data){
-                                          if (err) {
-                                            res.writeHead(404);
-                                            res.end("404 Not Found");
-                                          }
-                                          else {
-                                            res.writeHead(200, {
-                                              "Content-Type": "text/html"
-                                            });
+              function(err, data) {
+                      if (err) {
+                          res.writeHead(404);
+                          res.end("404 Not Found");
+                      }
+                      else {
+                          res.writeHead(200, {
+                            "Content-Type": "text/html"
+                          });
 
-      var reqHeaderString = JSON.stringify(req.headers, ['host', 'connection', 'accept'], 1);
-      reqHeaderString = reqHeaderString.replace(/\n/gi, "")
-                                       .replace(/[\"]/g, "");
+                          var reqHeaderString = JSON.stringify(req.headers,
+                                                  ['host', 'connection', 'accept'], 1);
+                          reqHeaderString = reqHeaderString.replace(/\n/gi, "")
+                                                           .replace(/[\"]/g, "");
 
-      var resHeaderString = res._header;
-      resHeaderString = resHeaderString.replace(/\n/gi, ", ")
-                                       .replace(/\r/gi, "")
-                                       .substring(0, resHeaderString.length - 4);
+                          var resHeaderString = res._header;
+                          resHeaderString =
+                                  resHeaderString.replace(/\n/gi, ", ")
+                                                 .replace(/\r/gi, "")
+                                                 .substring(0, resHeaderString.length - 4);
 
-      var myReqObject = {
-        "req.url" : req.url,
-        "req.method" : req.method,
-        "req.httpVersion" : req.httpVersion,
-        "req.headers" : reqHeaderString
-      }
+                          var myReqObject = {
+                            "req.url" : req.url,
+                            "req.method" : req.method,
+                            "req.httpVersion" : req.httpVersion,
+                            "req.headers" : reqHeaderString
+                          }
 
-      var myResObject = {
-        "res.statusCode" : res.statusCode,
-        "res.statusMessage" : res.statusMessage,
-        "res._header" : resHeaderString
-      }
+                          var myResObject = {
+                            "res.statusCode" : res.statusCode,
+                            "res.statusMessage" : res.statusMessage,
+                            "res._header" : resHeaderString
+                          }
 
-      var originalString = data;
-      var reqReplacer = JSON.stringify(myReqObject, null, 4);
-      var resReplacer = JSON.stringify(myResObject, null, 4);
+                          var originalString = data;
+                          var reqReplacer = JSON.stringify(myReqObject, null, 4);
+                          var resReplacer = JSON.stringify(myResObject, null, 4);
 
-      var newString = data.replace(/{{ req }}/, reqReplacer)
-                          .replace(/{{ res }}/, resReplacer);
+                          var newString = data.replace(/{{ req }}/, reqReplacer)
+                                              .replace(/{{ res }}/, resReplacer);
 
-      res.end(newString);
-    }
-  });
-});
+                          res.end(newString);
+                      } // end of else
+    }); // end of call back function and fs.readFile
+}); // end of http.createServer
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
