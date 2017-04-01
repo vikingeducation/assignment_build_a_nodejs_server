b8d9841ee12ee911a795d657e68fbfcf2f7879c1
Bill Hefty <billhefty@gmail.com>
2017-04-01 16:46:40 -0500

    send html as response

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const http = require('http')
+const fs = require('fs')
 
 const PORT = 3000
 const HOST = 'localhost'
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
+    fs.readFile('./public/index.html', 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(404)
+            res.end('404 Not Found')
+        }
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(data)
     })
-    res.end('Hello world!')
 })
 
 server.listen(PORT, HOST, () => {
