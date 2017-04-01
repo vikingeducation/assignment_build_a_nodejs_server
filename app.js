13759b3760ea9440e706c29a999668c51c709343
Bill Hefty <billhefty@gmail.com>
2017-04-01 16:59:13 -0500

    read and send header information

--- app.js
+++ app.js
@@ -13,7 +13,37 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             'Content-Type': 'text/html'
         })
-        res.end(data)
+
+        let reqHeader = JSON.stringify(req.headers, ['host', 'connection', 'accept', 1])
+        reqHeader = reqHeader.replace(/\n/gi, '')
+                             .replace(/[\"]/g, '')
+
+        let resHeader = res._header
+        resHeader = resHeader.replace(/\n/gi, ', ')
+                             .replace(/\r/gi, '')
+                             .substring(0, resHeader.length - 4)
+
+        let reqObject = {
+            'req.url': req.url,
+            'req.method': req.method,
+            'req.httpVersion': req.httpVersion,
+            'req.headers': reqHeader
+        }
+
+        let resObject = {
+            'res.statusCode': res.statusCode,
+            'res.statusMessage': res.statusMessage,
+            'res._header': resHeader
+        }
+
+        let originalData = data
+        let reqReplacer = JSON.stringify(reqObject, null, 2)
+        let resReplacer = JSON.stringify(resObject, null, 2)
+
+        let newData = data.replace(/{{ req }}/, reqReplacer)
+                          .replace(/{{ res }}/, resReplacer)
+
+        res.end(newData)
     })
 })
 
