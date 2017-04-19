47b0a6475500bbee2b4fc861c1b9fee0548133cf
Christian Florez <florez.christian@gmail.com>
2017-04-19 15:29:21 -0400

    add html rendering

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 - Whoops, we couldn't find anything there!");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
