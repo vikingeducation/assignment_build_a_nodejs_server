2d467a43001b62f7dca3108743436d085953a33e
Tyler Ketron <tketron@gmail.com>
2017-04-29 19:33:18 -0700

    serve html files instead of plain text

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content_Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
