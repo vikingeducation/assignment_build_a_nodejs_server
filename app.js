bda4c7d8d3dbd0a5160f0df823ed9a1479f395bd
Greg Filipczak <gregfilipczak@gmail.com>
2017-05-12 13:49:59 +0000

    reading basic html

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ var port = 3000;
 var host = '0.0.0.0';
 
 var server = http.createServer(function(req, res){
-  res.writeHead(200, {
-    "Content-Type": 'text/plain'
+  fs.readFile('./public/index.html', 'utf8', function(err, data){
+    if (err){
+      res.end('error of some kind')
+    } else {
+      res.writeHead(200, {
+        "Content-Type": 'text/html'
+      });
+      res.end(data);
+    }
   });
-  res.end('hello world!')
 });
 
 server.listen(port, host, function(){
   console.log(`Listening on http://${host}: ${port}`)
-})
+});
