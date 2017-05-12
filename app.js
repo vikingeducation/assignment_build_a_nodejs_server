7334cdb564f9c656f6555af102dc454187370e73
Greg Filipczak <gregfilipczak@gmail.com>
2017-05-12 20:23:48 +0000

    display req and res data in html

--- app.js
+++ app.js
@@ -5,10 +5,23 @@ var port = 3000;
 var host = '0.0.0.0';
 
 var server = http.createServer(function(req, res){
+  var request = {
+    'url':req.url,
+    'method':req.method,
+    'httpVersion':req.httpVersion,
+    'headers':req.headers
+  };
+  var results = {
+    'statusMessage': res.statusMessage,
+    'statusCode': res.statusCode,
+    'header': res._header
+  }
   fs.readFile('./public/index.html', 'utf8', function(err, data){
     if (err){
       res.end('error of some kind')
     } else {
+      data = data.replace("{{ req }}", JSON.stringify(request, null, 2));
+      data = data.replace("{{ res }}", JSON.stringify(results, null, 2));
       res.writeHead(200, {
         "Content-Type": 'text/html'
       });
