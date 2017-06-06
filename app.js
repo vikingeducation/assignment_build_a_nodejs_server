e8b15fe476ec14e643cf91830850debb9f8300cc
Luke E Summerscales <ke2uke@virginmedia.com>
2017-06-06 11:35:19 +0100

    Server running with HTML

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var http = require("http");
+var fs = require("fs");
 
 var host = "localhost";
 var port = 3000;
 
 var server = http.createServer(function(req, res){
-  res.statusCode = 200
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Howdy");
+  fs.readFile("./public/index.html", "utf8", function(err, data){
+    if (err) {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Error Page Not Found")
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, host, function() {
