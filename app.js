fa90b17b8b5909eb332c1e929b737a8e008798c6
Luke E Summerscales <ke2uke@virginmedia.com>
2017-06-06 13:51:14 +0100

    Result and Request displayed in HTML

--- app.js
+++ app.js
@@ -4,20 +4,33 @@ var fs = require("fs");
 var host = "localhost";
 var port = 3000;
 
-var server = http.createServer(function(req, res){
+const server = http.createServer(function(req, res){
   fs.readFile("./public/index.html", "utf8", function(err, data){
     if (err) {
       res.statusCode = 404;
-      res.setHeader("Content-Type", "text/plain");
+      res.setHeader("content-type", "text/plain");
       res.end("Error Page Not Found")
     } else {
       res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
+      res.setHeader("content-type", "text/html");
+      var details = {
+        url: req.headers["host"],
+        method: req.method,
+        httpVersion: req.httpVersion,
+        headers: req.headers
+      };
+      var details2 = {
+        statusMessage: res.statusMessage,
+        statusCode: res.statusCode,
+        header: res._header
+      };
+      data = data.replace(/{{ req }}/, JSON.stringify(details, null, 2));
+      data = data.replace(/{{ res }}/, JSON.stringify(details2, null, 2));
       res.end(data);
     }
   });
 });
-
+console.log();
 server.listen(port, host, function() {
   console.log(`Listening on http://${host}:${port}`)
 });
