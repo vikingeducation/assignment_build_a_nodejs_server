cf5769b85a96fb1275fab78a5a3dd0e2292a4e18
Andrew Chong <opheron@gmail.com>
2017-06-13 11:36:51 -0400

    Add functionality to read and send HTML file

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
 const host = 'localhost';
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello world!');
+  fs.readFile('public/index.html', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  })
+
 });
 
 server.listen(port, host, () => {
