3c58e7e71b7cb645a65d203f04c09151de4a1865
Andrew Chong <opheron@gmail.com>
2017-06-13 12:03:11 -0400

    Render select req and res data

--- app.js
+++ app.js
@@ -5,11 +5,26 @@ const port = 3000;
 const host = 'localhost';
 
 const server = http.createServer((req, res) => {
-  fs.readFile('public/index.html', (err, data) => {
+  fs.readFile('public/index.html', 'utf8', (err, data) => {
     if (err) {
       throw err;
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
+    const reqData = {
+      url: req.url,
+      method: req.method,
+      httpVersion: req.httpVersion,
+      headers: req.headers
+    };
+
+    const resData = {
+      statusMessage: res.statusMessage,
+      statusCode: res.statusCode,
+      _header: res._header
+    }
+    data = data.replace('{{ req }}', JSON.stringify(reqData, null, 2));
+    data = data.replace('{{ res }}', JSON.stringify(resData, null, 2));
+    console.log(data);
     res.end(data);
   })
 
