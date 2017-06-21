a12fc3ef8db12c5c6acd944fcb2a3152349bba19
Alex-Willenbrink <awillenbrink@gmail.com>
2017-06-21 09:58:08 -0500

    outputting html DONEZO

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 "use strict";
 
 const http = require("http");
+const fs = require("fs");
 
 let port = 3000;
 let host = "localhost";
 
 let server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    "Content-Type": "text/html"
-  });
-  res.end("Hello World!");
+  fs.readFile("./public/index.html", "utf8", function(err, data) {
+    if(err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  } );
+
+  //res.end("Hello World!");
 });
 
 server.listen(port, host, function() {
