2b2f63ccb553b0511c0046148e0800958ead74bf
Alex-Willenbrink <awillenbrink@gmail.com>
2017-06-21 12:12:04 -0500

    displayed request and response data

--- app.js
+++ app.js
@@ -12,9 +12,18 @@ let server = http.createServer(function(req, res) {
       res.writeHead(404);
       res.end("404 Not Found");
     } else {
+
       res.writeHead(200, {
         "Content-Type": "text/html"
       });
+      let reqArray = ["url", "method", "httpVersion", "headers"];
+      let storReq = JSON.stringify(req, reqArray, 2);
+
+      let resArray = ["statusMessage", "statusCode", "_header"];
+      let storRes = JSON.stringify(res, resArray, 2)
+
+      data = data.replace(/{{ req }}/, storReq);
+      data = data.replace(/{{ res }}/, storRes);
       res.end(data);
     }
   } );
