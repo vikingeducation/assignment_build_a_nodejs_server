f7209d0f28c114baa715fbac791a87a023e247e1
Benjamin Soung <soungbenjamin@gmail.com>
2017-06-29 16:27:40 -0400

    setup basic node app

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostName = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World!');
+});
+
+server.listen(port, hostName, () => {
+	console.log(`Server running at http://${hostName}:${port}/`);
+});
