f6efe81033ff23b5a9f307565551c9a7ea1a354f
Allen Anderson <dande313@byu.net>
2017-06-28 20:22:57 -0400

    Updated app.js to link to index.html

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var http = require('http');
 var port = 3000;
 var host = 'localhost';
+var fs = require('fs');
 
 var server = http.createServer(function(req,res){
-	res.writeHead(200,{
-		"Content-Type":'text/plain'
-	});
-	res.end("Hello World!!!");
+	fs.readFile('./public/index.html','utf8',function(err,data){
+		if(err){
+			res.writeHead(404);
+			res.end("This page under construction. Check back next week.")
+		}else{
+			res.writeHead(200,{
+				"Content-Type":'text/html'
+			});
+			res.end(data);
+		}
+	});		
 });
 
 server.listen(port,host,function(){
 	console.log("It's working! IT'S WORKING!!!")
 });
+
 
