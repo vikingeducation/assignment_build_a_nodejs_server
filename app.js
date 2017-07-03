a8cc1210097c7b6234f4cf0524ff7cc942a7dadd
Will Timpson <will.timpson@gmail.com>
2017-07-03 15:58:44 -0400

    HTML Server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const http = require('http');
 const port = 8000;
 const host = 'localhost';
@@ -8,11 +9,24 @@ const server = http.createServer(buildResponse);
 server.listen(port, host, announceLoad);
 
 function buildResponse(request, response) {
-  console.log('responding!')
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello, world!\n');
+
+  fs.readFile('./public/index.html', function(error, data) {
+    returnFile(error, data, request, response);
+  });
 }
+
+function returnFile(error, data, request, response) {
+  if (error) {
+    response.writeHead(404);
+    response.end('404: File not found.');
+  } else {
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    response.end(data);
+  }
+}
+
 
 function announceLoad() {
   console.log(`Server running at http://${host}:${port}/`)
