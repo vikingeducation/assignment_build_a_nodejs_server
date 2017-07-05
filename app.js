a95bd147467d068b4b33c891695589a4b3c3564a
Brandon <bparkerproductions@gmail.com>
2017-07-04 19:55:23 -0500

    basic server up

---
+++ app.js
@@ -0,0 +1,24 @@
+var http = require("http");
+var fs = require("fs");
+
+var port = "4000";
+var host = "127.0.0.1";
+
+var server = http.createServer(function(req,res){
+	fs.readFile("./public/index.html","utf8", function(err,data){
+		if(err){
+			res.writeHead(404);
+			res.end("404 not found");
+		}
+		else{
+			res.writeHead(200, {
+				"Content-Type": "text/html"
+			});
+			res.end(data);
+		}
+	})
+});
+
+server.listen(port, host, function(){
+	console.log(`Listening on port ${port} and host ${host}`);
+})
