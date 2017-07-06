10d56653b6dc404cfb056b6c63cfbb63607fb537
Stephanie Barker <song_mage@yahoo.com>
2017-07-06 13:25:18 -0400

    Server now serves index.html

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const hostname = "localhost"
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/plain");
-	res.end("Hello World!")
+	fs.readFile("./public/index.html", "utf8", function(error, data) {
+		if (error) {
+			res.writeHead(404);
+			res.end("404 File Not Found");
+		} else {
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "text/html")
+			res.end(data);
+		}
+	})
 });
 
 server.listen(port, hostname, function() {
