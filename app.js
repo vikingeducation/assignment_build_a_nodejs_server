2cb6f648293454f6b678e3acee61c84bee155987
Stephanie Barker <song_mage@yahoo.com>
2017-07-06 14:19:34 -0400

    used a template to render object variables

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-const http = require("http");
 const fs = require("fs");
+
+const express = require("express");
+const app = express();
 
 const hostname = "localhost"
 const port = 3000
 
-const server = http.createServer(function(req, res) {
-	fs.readFile("./public/index.html", "utf8", function(error, data) {
-		if (error) {
-			res.writeHead(404);
-			res.end("404 File Not Found");
-		} else {
-			res.statusCode = 200;
-			res.setHeader("Content-Type", "text/html")
-			res.end(data);
-		}
-	})
+app.set("view engine", "hbs");
+
+app.get("/", function(req, res) {
+	res.statusMessage = "Everything's fine?"
+	reqSimple = {
+		"url": req.url,
+		"method": req.method,
+		"httpVersion": req.httpVersion,
+		"headers": req.headers
+	};
+
+	resSimple = {
+		"statusMessage": res.statusMessage,
+		"statusCode": res.statusCode,
+		"_header": res._header
+	};
+
+	res.render("index", {
+		req: JSON.stringify(reqSimple, null, 2),
+		res: JSON.stringify(resSimple, null, 2)
+	});
 });
 
-server.listen(port, hostname, function() {
+app.listen(port, hostname, function() {
 	console.log(`Server is running at http://${hostname}:${port}`);
 });
