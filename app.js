6ec031cba6000583cec214a8fc5e79c0549e2076
Ian Halverson <halversoncode@gmail.com>
2017-07-07 13:55:32 -0400

    serving up some delicious html

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
 const hostname = "localhost"
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const server = http.createServer(function(req, res){
+    fs.readFile("./public/index.html", "utf8", function(err, data) {
+      if(err){res.writeHead(404); res.end("404 not found");}
+      else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      }
+    })
 });
 
 server.listen(port, hostname, function() {
