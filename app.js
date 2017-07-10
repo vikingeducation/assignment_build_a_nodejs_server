315b3e2c0d085e903ddf1bda23cc2f220316518a
Ian Halverson <halversoncode@gmail.com>
2017-07-10 11:52:10 -0400

    displayed that funky req/res data

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const port = 3000;
 const hostname = "localhost"
 
 const server = http.createServer(function(req, res){
-    fs.readFile("./public/index.html", "utf8", function(err, data) {
+      fs.readFile("./public/index.html", "utf8", function(err, data) {
       if(err){res.writeHead(404); res.end("404 not found");}
       else {
+        var htmlOut = data;
+        var reqData = JSON.stringify(req.url)+JSON.stringify(req.method)
+          +JSON.stringify(req.httpVersion)+JSON.stringify(req.headers);
+        var resData = JSON.stringify(res.statusMessage)
+          +JSON.stringify(res.statusCode)+JSON.stringify(res._header);
+        htmlOut = htmlOut.replace("{{ req }}", reqData);
+        htmlOut = htmlOut.replace("{{ res }}", resData);
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(data);
+        res.end(htmlOut);
       }
     })
 });
