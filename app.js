76c51edaceaa0b199b79c9215694bc1655f51acf
Jeff Bernstein <jeffreybbernstein@gmail.com>
2017-07-25 20:47:41 -0700

    output html

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
 
-const host = 'localhost';
-const port = 3000;
+var port = 3000;
+var host = 'localhost';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var server = http.createServer(function(req, res) {
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
-server.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}/`);
+server.listen(port, host, function() {
+  console.log(`Listening at http://${host}:${port}`);
 });
