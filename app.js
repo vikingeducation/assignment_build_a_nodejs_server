23ede7b8dd2a550cb8de2a769db5baa44dc84318
Jeff Bernstein <jeffreybbernstein@gmail.com>
2017-07-25 21:14:17 -0700

    finished displaying the request and response data

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ var server = http.createServer(function(req, res) {
       res.writeHead(404);
       res.end("404 Not Found");
     } else {
+      var reqData = JSON.stringify(req.url, null, 2) + JSON.stringify(req.method, null, 2) +
+        JSON.stringify(req.httpVersion, null, 2) + JSON.stringify(req.headers, null, 2);
+      var resData = JSON.stringify(res.statusMessage, null, 2) +
+        JSON.stringify(res.statusCode, null, 2) + JSON.stringify(res._header, null, 2);
+      var htmlData = data.replace("{{ req }}", reqData).replace("{{ res }}", resData);
       res.writeHead(200, {
         "Content-Type": "text/html"
       });
-      res.end(data);
+      res.end(htmlData);
     }
   });
 });
