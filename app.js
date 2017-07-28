77984e6d90f2e6bd5e1bb0272ab30bfa9204c6e7
Timi <oblacishere@gmail.com>
2017-07-28 15:26:25 +0100

    final push
    added a form to my html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const port = 3000;
-const hostname = 'localhost'
+const hostname = 'localhost';
 
 http.createServer(function(req,res) {
 	fs.readFile('./public/index.html', 'utf8', function(err, data) {
@@ -9,9 +9,27 @@ http.createServer(function(req,res) {
 			res.writeHead(404);
 			res.end('404 Not Found');
 		} else {
-			res.writeHead(200, {
-				"Content-Type": "text/html"
+			let require = {
+				"url": req.url,
+				"method" : req.method,
+				"httpVersion": req.httpVersion,
+				"headers": req.headers
+			};
+
+
+			require = JSON.stringify( require, null, 2);
+
+			let respond = {};
+			["statusMessage", "statusCode", "_header"].forEach( function(element) {
+				respond[element] = res[element];
 			});
+
+			respond = JSON.stringify(respond, null, 2);
+
+			res.writeHead(200, {"Content-Type": "text/html"});
+
+			data = data.replace("{{ req }}", require);
+			data = data.replace("{{ res }}", respond);
 			res.end(data);
 		}
 	});
