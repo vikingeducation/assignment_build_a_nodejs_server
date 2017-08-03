a4691479fb3275a3c4df2c38e5ed58f1d8a235ab
John Whittington <johncw90@yahoo.com>
2017-08-03 12:59:14 -0500

    Implemented HTML output

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 
 var port = 3000;
 var hostname = 'localhost';
 
 var server = http.createServer(function(req,res) {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World!');
+	
+	fs.readFile('./public/index.html', 'utf8', function(err, data){
+		if (err) {
+			res.writeHead(404);
+			res.end('404 Not Found');
+		} else {
+			res.writeHead(200, {
+				'Content-Type': 'text/html'
+			});
+			res.end(data);
+		}
+
+	});
 
 });
 
