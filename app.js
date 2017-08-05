9a41658d751065ea0226362548e37bbc34aff34e
anne <richardson.ae@gmail.com>
2017-08-05 12:08:17 -0500

    Enable server to run

--- app.js
+++ app.js
@@ -10,8 +10,13 @@ var port = 3000;
 // create a server
 var server = http.createServer(function(request, response){
   response.writeHead(200, {
-    "Content-Type": "text/html"
+    "Content-Type": "text/plain"
   });
   response.end('hello world');
 });
 
+
+// enable the server to run
+server.listen(port, host, function(){
+  console.log(`Listening at http://${ host }:${ port }`);
+});
