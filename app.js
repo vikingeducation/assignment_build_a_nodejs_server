d9206dd07b3a383ee2bfe25c3ced831b8cdc3859
anne <richardson.ae@gmail.com>
2017-08-05 12:00:01 -0500

    Create a simple server'

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ var fs = require('fs');
 var host = 'localhost';
 var port = 3000;
 
+
+// create a server
+var server = http.createServer(function(request, response){
+  response.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+  response.end('hello world');
+});
+
