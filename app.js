7ae10bb21dd1c411a145011838d28a20fdc1290b
Paul Savage <p.a.savage@btinternet.com>
2017-08-07 12:21:30 +0100

    Set up HTTP server

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var hostname = 'localhost';
+var port = 3000;
+
+var server = http.createServer(function(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!');
+});
+
+server.listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
