46e64a5590157f0f00dd69a91df3414c57be3b36
Paul Savage <p.a.savage@btinternet.com>
2017-08-07 12:28:06 +0100

    Outputting HTML

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 var hostname = 'localhost';
 var port = 3000;
 
 var server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, function() {
