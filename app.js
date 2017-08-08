594ea591ab3deb9014988a0e9cb5a26167ed9a21
anne <richardson.ae@gmail.com>
2017-08-07 19:54:28 -0500

    Output JSON object info into strings for the view

--- app.js
+++ app.js
@@ -9,14 +9,34 @@ var port = 3000;
 
 // create a server
 var server = http.createServer(function(request, response){
-  fs.readFile('./public/index.html', 'utf8', function(err, data){
+  // set up simple server
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
     if (err) {
       response.writeHead(404);
-      response.end("Whoops! That page is nowhere to be found.");
+      response.end("404 Whoops! That page is nowhere to be found.");
     } else {
-      response.writeHead(200, {
-        "Content-Type": "text/html"
-      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+
+      // capture incoming request data in an object
+      var requestObj = {
+        "request.url": request.url,
+        "request.method": request.method,
+        "request.httpVersion": request.httpVersion,
+        "request.headers": request.headers
+      }
+
+      // capture response data in an object
+      var responseObj = {
+        'response.statusCode': response.statusCode,
+        'response.statusMessage': response.statusMessage,
+        'response._header': response._header
+      }
+
+      // output JSON objects info into strings
+      var requestOutput = JSON.stringify( requestObj, null, 2 );
+      var responseOutput = JSON.stringify( responseObj, null, 2 );
+
+      // pass the data to the view
       response.end(data);
     }
   });
