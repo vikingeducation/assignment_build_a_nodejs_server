b9a755a34ffa449e287255173597ad330389bd18
Chuck Michael <chuckqs@yahoo.com>
2017-08-09 14:58:50 -0400

    Completed Display the Request and Response Data

--- app.js
+++ app.js
@@ -5,6 +5,30 @@ var filePath = "./public/index.html";
 var port = 3000;
 var host = 'localhost';
 
+var replacePreWith = function (req, res, data){
+  var reqPropertiesToFilter = ["url", "method", "httpVersion", "headers"];
+  var resPropertiesToFilter = ["statusMessage", "statusCode", "_header"];
+  var reqPropertiesValues = {};
+  var resPropertiesValues = {};
+  var jsonStringReqValues;
+  var jsonStringResValues;
+
+  for (var i = 0; i< reqPropertiesToFilter.length; i++) {
+    reqPropertiesValues[reqPropertiesToFilter[i]] = req[reqPropertiesToFilter[i]];
+  }
+  for (var i = 0; i< resPropertiesToFilter.length; i++) {
+    resPropertiesValues[resPropertiesToFilter[i]] =res[resPropertiesToFilter[i]];
+  }
+
+  jsonStringReqValues = JSON.stringify(reqPropertiesValues, null, 2);
+  jsonStringResValues = JSON.stringify(resPropertiesValues, null, 2);
+
+  data = data.replace(/req/i, jsonStringReqValues);
+  data = data.replace(/res/i, jsonStringResValues);
+
+  return data;
+};
+
 var server = http.createServer (function(req,res){
   fs.readFile(filePath, 'utf8', function (err,data){
     if (err){
@@ -14,7 +38,7 @@ var server = http.createServer (function(req,res){
       res.writeHead(200,{
         "Content-Type": "text/html"
       });
-      res.end(data);
+      res.end(replacePreWith( req, res, data));
     }
   });
 });
