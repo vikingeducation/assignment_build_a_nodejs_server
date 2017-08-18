d73fdf2520c32bf957eac06e87dec65f909bc8ce
Coelacanth7 <galford151@gmail.com>
2017-08-18 14:32:06 -0700

    finised displaying data

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 
-
-var port = 3000;
-var host = 'localhost';
+const port = 3000;
+const host = 'localhost';
 
 var server = http.createServer(function(req, res){
+
   fs.readFile('./public/index.html', 'utf8', function(err, data){
     if (err) {
       res.writeHead(404);
@@ -14,7 +14,34 @@ var server = http.createServer(function(req, res){
       res.writeHead(200, {
         "Content-Type": "text/html"
       });
-      res.end(data);
+
+      //map the properties because req and res are huge circular objects
+      var reqProperties = {};
+
+        reqProperties.url = req.url;
+        reqProperties.method = req.method;
+        reqProperties.httpVersion = req.httpVersion;
+        reqProperties.headers = req.headers;
+
+      var resProperties = {};
+
+        resProperties.statusMessage = res.statusMessage,
+        resProperties.statusCode = res.statusCode,
+        resProperties._header = res._header
+
+      //replace res and req with JSON object data
+      data = data.replace(//replace
+        "{{ req }}",//this
+        JSON.stringify(reqProperties, null, 2)//with this
+      );
+
+      data = data.replace(
+        "{{ res }}",
+        JSON.stringify(resProperties, null, 2)
+      );
+
+      res.end(data);//end
+
     }
   });
 });
