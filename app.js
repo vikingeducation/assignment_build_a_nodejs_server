6f604802db9bc218a7bf9a42ec506b0f2befe501
Jennifer Louie <louie.jen@gmail.com>
2017-08-23 23:38:34 -0700

    Completed outputting HTML

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 var port = 3000;
 var host = 'localhost';
 
 var server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, host, function() {
