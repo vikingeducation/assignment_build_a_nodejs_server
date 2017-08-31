55181267100d5612f9c36e4910252f45e24cfa4d
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-08-31 10:48:52 +0200

    outputting HTML

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 var port = 3000;
 var host = 'localhost';
 
 var server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end('404 Page Not Found');
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data);
+    };
   });
-  res.end('Hello World!');
 });
 
 server.listen(port, host, function() {
