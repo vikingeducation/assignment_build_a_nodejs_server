7135f3c93287ce69b38f9da59655145093bad198
Aaron Saloff <aaronsaloff@gmail.com>
2017-09-02 23:15:59 -0400

    Displaying the Request and Response Data - completed

--- app.js
+++ app.js
@@ -6,10 +6,32 @@ var host = 'localhost';
 
 var server = http.createServer(function(req, res) {
   fs.readFile('./public/index.html', 'utf8', function(err, data) {
-    res.writeHead(200, {
-      "Content-Type": 'text/html'
-    });
-    res.end(data);
+    if (err) {
+      res.writeHead(404);
+      res.end('There was an error');
+    } else {
+      res.writeHead(200, {
+        "Content-Type": 'text/html'
+      });
+
+      var smallReq = {
+        url: req.url,
+        method: req.method,
+        httpVersion: req.httpVersion,
+        headers: req.headers
+      };
+
+      var smallRes = {
+        statusMessage: res.statusMessage,
+        statusCode: res.statusCode,
+        _header: res._header
+      };
+
+      var replaced = data.replace('{{ req }}', JSON.stringify(smallReq, null, 2))
+        .replace('{{ res }}', JSON.stringify(smallRes, null, 2));
+
+      res.end(replaced);
+    }
   });
 });
 
