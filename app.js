f36e051fae3df981ad368a58e044e8ce0fa81858
maddiereddy <maddiereddy@gmail.com>
2017-09-06 10:04:01 -0700

    completed outputting html

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 'use strict';
 
 let http = require("http");
+let fs = require("fs");
 
 let host = "localhost";
 let port = 3000;
 
 let server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-  	"Content-Type": "text/plain"
+  // res.writeHead(200, {
+  // 	"Content-Type": "text/plain"
+  // });
+  // res.end("Hello World!");
+
+  fs.readFile("./public/index.html", "utf8", function(err, data){
+  	if (err) {
+  	  res.writeHead(404);
+  	  res.end("404 Not Found");
+  	} else {
+  	  res.writeHead(200, {"Content-Type": "text/html"});
+  	  res.end(data);
+  	}
   });
-  res.end("Hello World!");
 });
 
 server.listen(port, host, function() {
