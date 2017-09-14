0f07049ea1e76b8fd0dcc4f2a9ec5cba034c9767
Jennifer Louie <louie.jen@gmail.com>
2017-09-14 13:00:22 -0700

    Completed displaying of request and response data

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 
-var port = 3000;
-var host = 'localhost';
+const port = 3000;
+const host = 'localhost';
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer((req, res) => {
   fs.readFile('./public/index.html', 'utf8', function(err, data) {
     if (err) {
       res.writeHead(404);
@@ -13,7 +13,12 @@ var server = http.createServer(function(req, res) {
       res.writeHead(200, {
         "Content-Type": "text/html"
       });
-      res.end(data);
+
+      let shallowViewRequest = createViewReq(req);
+      let shallowViewResponse = createViewRes(res);
+      let newHtml = data.replace('{{ req }}', JSON.stringify(shallowViewRequest));
+      newHtml = newHtml.replace('{{ res }}', JSON.stringify(shallowViewResponse));
+      res.end(newHtml);
     }
   });
 });
@@ -21,3 +26,20 @@ var server = http.createServer(function(req, res) {
 server.listen(port, host, function() {
   console.log(`Listening at http://${ host }:${ port }`);
 });
+
+function createViewReq(req) {
+  return {
+    url : req.url,
+    method : req.method,
+    httpVersion : req.httpVersion,
+    headers : req.headers
+  };
+}
+
+function createViewRes(res) {
+  return {
+    statusMessage : res.statusMessage,
+    statusCode : res.statusCode,
+    _header : res._header
+  };
+}
