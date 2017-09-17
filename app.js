b5c178aab1c310697be17a6a1e721862bd05a596
Jared Gebel <jaredjgebel@gmail.com>
2017-09-17 19:44:22 -0400

    Complete displaying request and response data

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ var server = http.createServer((req, res) => {
       res.end("404 Not Found");
     } else {
       res.writeHead(200, { "Content-Type": "text/html" });
+
+      var reqObject = {
+        url: req.url,
+        method: req.method,
+        httpVersion: req.httpVersion,
+        headers: req.headers
+      };
+      var resObject = {
+        statusMessage: res.statusMessage,
+        statusCode: res.statusCode,
+        _header: res._header
+      };
+
+      var reqString = JSON.stringify(reqObject, null, 2);
+
+      var resString = JSON.stringify(resObject, null, 2);
+
+      data = data.replace(/{ req }/i, reqString);
+      data = data.replace(/{ res }/i, resString);
       res.end(data);
     }
   });
@@ -21,3 +40,14 @@ server.listen(port, host, function() {
     `The server is now running and listening at the address http://${host}:${port}`
   );
 });
+
+/*
+req.url
+req.method
+req.httpVersion
+req.headers
+
+res.statusMessage
+res.statusCode
+res._header
+*/
