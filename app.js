6ef7410324bd31a48824844b9f1b6fe9f3939ab7
Jared Gebel <jaredjgebel@gmail.com>
2017-09-17 17:08:16 -0400

    Set up index.html

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const host = "127.0.0.1";
 const port = 3000;
 
 var server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+  fs.readFile("./public/index.html", "utf8", function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, host, function() {
