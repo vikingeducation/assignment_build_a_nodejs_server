b0b80c62241f8c35d0d0b44e59e97d85af877ab8
DC <heyyodc-git@yahoo.com>
2017-09-20 14:13:58 -0700

    completed 'outputting html' section

--- app.js
+++ app.js
@@ -5,14 +5,28 @@
 'use strict';
 
 const http = require('http');
+const fs = require('fs');
 
 const hostname = 'localhost';
 const port = 3001;
 
 const server = http.createServer(function(req,res) {
+	fs.readFile(__dirname + '/public/index.html', 'utf8', function(err,data) {
+		if (err) {
+			res.writeHead(404);
+			res.end("File not found");
+		} else {
+			res.writeHead(200,
+				{'Content-Type': 'text/html'});
+			res.end(data);
+		};
+	});	
+
+/*
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
 	res.end('Hello World!\n');
+*/	
 });
 
 server.listen(port, hostname, function () {
