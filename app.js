9ea909b9528368b1a47867061f54d6759dc9d42b
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-09-27 16:12:46 -0400

    outputting HTML section complete

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 "use strict";
 
-const http = require('http');
+const http = require('http'),
+	fs = require('fs');
 
 let server = http.createServer((req, res) => {
-	
-	res.writeHead(200, {
-		"Content-Type": "text/html"
+	fs.readFile('./public/index.html', 'utf8', function(err, data) {
+		if (err) {
+			res.writeHead(404);
+			res.end("404 - Not Found");
+		} else {
+			res.writeHead(200, {
+				"Content-Type": "text/html"
+			});
+			res.end(data);
+		}
 	});
-	res.end("<h1>Hello world!</h1>");
 });
 
 server.listen(3000, 'localhost', function() {
