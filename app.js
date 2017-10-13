965189b66dac60d781d3922a01482036b59df1e7
Visiona <darbis@gmail.com>
2017-10-13 20:31:05 +0100

    Edited server so it sends html page in response

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var http = require('http');
+var fs = require('fs');
 
 var port = 3000;
 var host = 'localhost';
 
 var server = http.createServer( function(req, res) {
-    res.writeHead(200, {
-      "Content-Type": 'text/html'
-    });
-    res.end('Hello World!');
+  fs.readFile( './public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end('Page not Found 404');
+    } else {
+      res.writeHead(200, {
+        "Content-Type": 'text/html'
+      });
+      res.end(data);
+    }
+  })
 });
 
 server.listen(port, host, function() {
