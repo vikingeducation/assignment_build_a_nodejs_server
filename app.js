e31dfb99f78f0371b343b130c39b31b3c64b0da3
Visiona <darbis@gmail.com>
2017-10-13 20:23:14 +0100

    Created small node.js web server

---
+++ app.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var port = 3000;
+var host = 'localhost';
+
+var server = http.createServer( function(req, res) {
+    res.writeHead(200, {
+      "Content-Type": 'text/html'
+    });
+    res.end('Hello World!');
+});
+
+server.listen(port, host, function() {
+  console.log(`Listening at http://${ host }:${ port }/`);
+});
