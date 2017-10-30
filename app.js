5a04d3dd27a677e8b23d305d444d845c251739c4
Hai Nguyen <memyselfandhai@gmail.com>
2017-10-30 17:35:17 -0500

    request & response data displaying

--- app.js
+++ app.js
@@ -8,14 +8,21 @@ var host = 'localhost';
 
 var server = http.createServer(function (req, res) {
   fs.readFile('./public/index.html', 'utf8', function (err,data) {
+    request_obj = {URL: req.url, METHOD: req.method,
+                  'HTTP VERSION':req.httpVersion, HEADERS: req.headers};
+    result_obj = {STATUS:res.statusMessage, CODE:res.statusCode,
+                  HEADER:res._header}
+
     if (err) {
-      res.writeHead(404);
-      res.end("404 Not Found");
+        res.writeHead(404);
+        res.end("404 Not Found");
     } else {
-      res.writeHead(200, {
-        "Content-Type" : "text/html"
-      });
-      res.end(data);
+        res.writeHead(200, {
+          "Content-Type" : "text/html"
+        });
+        data = data.replace('{{ req }}', JSON.stringify(request_obj, null, 5));
+        data = data.replace('{{ res }}', JSON.stringify(result_obj, null, 5));
+        res.end(data);
     }
   })
 });
