de59a4a41c9d67aa4e7efcbc37d984b821e5afbf
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-02 23:30:39 -0400

    html file served

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const http = require('http');
+const fs = require('fs');
 
 let port = 3000;
 let host = 'localhost';
 
 let server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
+  fs.readFile('./public/index.html', 'utf8', (err, data) => {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.end(data);
   });
-  res.end('Hello World!');
 });
 
 server.listen(port, host, () => {
