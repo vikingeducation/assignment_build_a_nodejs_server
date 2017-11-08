0d6c03e1e5ef718ecaf5eca5344b1e8744d10693
Lakshmi <lakshmi.nishthala@gmail.com>
2017-11-07 20:05:44 -0800

    Node js sends back HTML file

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-//Node server sends back "Hello world!" :) - (this code works!)
 var http = require('http');
+var fs = require('fs');
 
 
 var hostname = 'localhost';
 var port = 3000;
 
+////Node server sends back "Hello world!" :) - (this code works!)
+// var server = http.createServer((req, res)=>{
+//   res.writeHead(200, {
+//     'Content-Type':'text/plain'
+//   });
+//   res.end("Hello World!");
+// });
+//
+// server.listen(port, hostname, ()=>{
+//   console.log(`Server is listening at ${hostname}:${port}`);
+// });
 var server = http.createServer((req, res)=>{
-  res.writeHead(200, {
-    'Content-Type':'text/plain'
+  fs.readFile('./public/index.html', 'utf-8', function(err, data){
+    if(err){
+      res.writeHead(404);
+      res.end("File Not Found");
+    }else{
+      res.writeHead(200,{
+        'Content-Type':'text/html'
+      });
+      res.end(data);
+    }
   });
-  res.end("Hello World!");
 });
-
 server.listen(port, hostname, ()=>{
-  console.log(`Server is listening at ${hostname}:${port}`);
-});
+   console.log(`Server is listening at ${hostname}:${port}`);
+ });
