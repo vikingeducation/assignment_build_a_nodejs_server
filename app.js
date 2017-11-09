bc3a489e86a647e8a88fc3404dd0e2b98e38423b
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-08 23:42:29 -0500

    reads and outputs html from public folder

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ var port=3000;
 var host='localhost';
 
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var server = http.createServer((req, res) => {
+  fs.readFile('./public/index.html',function(err, data){
+    if (err){
+      res.writeHead(404);
+      res.end("File not Found");
+    }
+    else{
+      res.writeHead(200,{
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, host, function(){
