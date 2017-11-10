2d1a44304b8c2c407b9e29b7c5ded331cb01c12f
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-09 22:06:55 -0500

    add form for submitting data

--- app.js
+++ app.js
@@ -6,16 +6,33 @@ var host='localhost';
 
 
 var server = http.createServer((req, res) => {
-  fs.readFile('./public/index.html',function(err, data){
+  fs.readFile('./public/index.html', 'utf-8', function(err, data){
     if (err){
       res.writeHead(404);
       res.end("File not Found");
     }
     else{
+      var reqObj={
+        url:req.url,
+        method:req.method,
+        version:req.httpVersion,
+        header:req.headers
+      };
+      var reqString=JSON.stringify(reqObj, null, 2);
       res.writeHead(200,{
         "Content-Type": "text/html"
       });
+      data=data.replace("{{ req }}", reqString);
+      var resObj = {
+        statusMessage:res.statusMessage,
+        statusCode:res.statusCode,
+        _header:res._header
+      };
+      var resString=JSON.stringify(resObj, null, 2);
+      data=data.replace("{{ res }}", resString);
       res.end(data);
+
+
     }
   });
 });
