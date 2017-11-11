34b883bb732e9d2737f578e576111ae4f26edb8a
Johann <baptistajohann@gmail.com>
2017-11-11 12:23:51 -0800

    Request/Response working

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ let fs = require('fs');
 let port = 3000;
 let host = 'localhost';
 
+let wantedReqProperties = ["url", "method", "httpVersion", "headers"];
+let wantedResProperties = ["statusMessage", "statusCode", "_header"];
+
 let server = http.createServer((req, res) => {
   fs.readFile('./public/index.html', 'utf8', (err, data) => {
     if (err) {
@@ -14,7 +17,18 @@ let server = http.createServer((req, res) => {
       res.writeHead(200, {
         "Content-Type":"text/html"
       });
-      res.end(data);
+
+      let request = wantedReqProperties.map((property) => {
+        return { [property] : req[property] };
+      });
+      let response = wantedResProperties.map((property) => {
+        return { [property] : res[property] };
+      });
+
+      let newData = data.replace("{{ req }}", JSON.stringify(request))
+                        .replace("{{ res }}", JSON.stringify(response));
+
+      res.end(newData);
     }
   });
 });
