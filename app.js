4cf5262073eab9966d6d9a3dd7e7b2a8e893bd10
tzvi <tzvika200@yahoo.com>
2017-11-17 07:30:51 -0500

    all working

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ let fs = require('fs');
 let port = 3000;
 let host = 'localhost';
 
-
-
 let server = http.createServer((req, res) => {
 	fs.readFile('./public/index.html', 'utf-8', function( err, data ) {
+		
 		res.writeHead (200, {'Content-Type': 'text/html' });
-		res.end(data);
+		
+		res.end(
+			data
+			 .replace(/{{ res }}/, JSON.stringify(res.statusMessage+" "+res.statusCode+" "+res._header, null, 2) )
+			 .replace(/{{ req }}/, JSON.stringify(req.url+" "+req.method+" "+req.httpVersion+" "+req.headers, null, 2))
+		);
 	});
 })
 
