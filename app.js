721f3b01d323ee75a910000dbbd061df0a5df6a4
ksc23 <ksc.main@gmail.com>
2018-01-04 01:45:24 -0800

    Add fs to read in html

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = '3000';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  fs.readFile('./public/index.html', 'utf-8', (err, data) => {
+    if(err) {
+      res.statusCode = 404;
+      res.end('404 Not Found');
+    } else {
+      res.statusCode = 200; 
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
