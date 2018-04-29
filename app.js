6fd613b30728616b00627b8d40eede56ce42f85a
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-28 22:55:09 -0400

    basic web server setup

--- app.js
+++ app.js
@@ -3,4 +3,47 @@
   cd Documents/Viking/Node/node_server
 */
 
-require('http');
+const http = require(`http`);
+
+const hostname = `127.0.0.1`;
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader(`Content-Type`, `text/plain`);
+  res.end(`Hello World\n`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// spacing
