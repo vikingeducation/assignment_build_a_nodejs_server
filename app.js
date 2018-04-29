8df92ddacedf8464c24f5243e68bc5ca0c628969
Brennan_Fulmer <31666354+BrennanFulmer@users.noreply.github.com>
2018-04-28 23:40:51 -0400

    slightly more advanced node server

--- app.js
+++ app.js
@@ -3,15 +3,29 @@
   cd Documents/Viking/Node/node_server
 */
 
-const http = require(`http`);
+const http = require(`http`),
+      fs = require(`fs`),
+      hostname = `127.0.0.1`,
+      port = 3000;
 
-const hostname = `127.0.0.1`;
-const port = 3000;
-
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader(`Content-Type`, `text/plain`);
   res.end(`Hello World\n`);
+});*/
+
+const server = http.createServer((req, res) => {
+  fs.readFile(`public/index.html`, `utf8`,(err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end(`404 File Not Found`);
+    } else {
+      res.writeHead(200, {
+        "Content-Type": `text/html`
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
