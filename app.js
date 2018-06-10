e701fdbe2ff4c2cb7072724b4c721c8aecc7cfa5
Megan Taylor <mtaylor@megantaylor.org>
2018-06-10 14:30:28 -0400

    now with HTML

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const host = '127.0.0.1';
 const port = 3000;
 
 var server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  fs.readFile('./public/index.html', 'utf8', function(err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, host, function() {
