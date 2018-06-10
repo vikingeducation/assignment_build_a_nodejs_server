e109595754356b24c4af77284fbc7a29f0764149
Megan Taylor <mtaylor@megantaylor.org>
2018-06-10 14:25:10 -0400

    hello world server

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 3000;
+
+var server = http.createServer(function(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, host, function() {
+  console.log(`Listening at http://${ host }:${ port }`);
+});
