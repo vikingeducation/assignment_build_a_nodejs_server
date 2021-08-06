87214a6136c22830b4db315f5bbba1e14ca54e58
Jurgen n <jurgen.gjoncari@gmail.com>
2021-08-06 17:40:21 +0200

    Display the Request and Response Data

--- app.js
+++ app.js
@@ -3,35 +3,34 @@ var fs = require("fs");
 
 var path = "./public/index.html";
 
-var server = http.createServer
-(
-	(req, res) =>
-	{
-		fs.readFile
-		(
-			path, (err, data) =>
-			{
-				if (err)
-				{
-					res.end("404 file not found");
-				}
-				else
-				{
-					res.end(data);
-				}
-			}
-		)
-	}
-);
+var server = http.createServer((req, res) => {
+	fs.readFile(path, "utf8", (err, data) => {
+		if (err) {
+			res.end("404 file not found");
+		} else {
+			res.setHeader("Content-type", "text/html");
+			reqFiltered = {
+				"url": req.url,
+				"method": req.method,
+				"httpVersion": req.httpVersion,
+				"headers": req.headers
+			};
+			resFiltered = {
+				"statusMessage": res.statusMessage,
+				"statusCode": res.statusCode,
+				"_header": res._header
+			};
+			data = data.replace("{{ req }}", JSON.stringify(reqFiltered, null, 2));
+			data = data.replace("{{ res }}", JSON.stringify(resFiltered, null, 2));
+			res.end(data);
+		}
+	});
+});
 
 var host = "localhost";
 var port = 3000;
 
-server.listen
-(
-	port, host, () =>
-	{
-		console.log(`Listening at http://${host}:${port}`);
-	}
-);
+server.listen(port, host, () =>	{
+	console.log(`Listening at http://${host}:${port}`);
+});
 
